76ce0d8331f550881c11b51c423d083f6b4d0ec4
elduderinow <yarrut.franken@gmail.com>
2021-08-05 13:27:21 +0200

    created routes and styled friend overview and friend page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './routes/user/user.component';
+import { FriendComponent } from './routes/friend/friend.component';
+import { OverviewComponent } from './routes/overview/overview.component';
+import { LoginComponent } from './routes/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    FriendComponent,
+    OverviewComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
